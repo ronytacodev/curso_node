ea3aba098fa6ec3afba9e6cbcadcfa31fd12670b
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-02 03:32:19 -0500

    terminando archivos estaticos

--- src/app.js
+++ src/app.js
@@ -5,11 +5,20 @@
 const express = require('express')
 const {route} = require('./routers/index.routers')
 const app = express()
+const path = require('path')
 
 // ROUTES
 // const routes =require('./routers/index.routers')
 // app.use(routes)
 app.use(require('./routers/index.routers'))
+
+// STATIC FILES
+app.use(express.static('../public'))
+// console.log(__dirname)
+
+app.use((req, res) => {
+    res.sendFile('index.html')
+})
 
 app.listen(3000, () => {
     console.log('Servidor a la espera de conexiones')
