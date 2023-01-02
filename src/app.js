557e1fc44378adea7b9680b7266ca509d74e2557
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-02 11:08:56 -0500

    usando motor de plantilla pug

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const {route} = require('./routers/index.routers')
 const app = express()
 const path = require('path')
 
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'views'))
+
 // ROUTES
 // const routes =require('./routers/index.routers')
 // app.use(routes)
@@ -17,7 +20,7 @@ app.use(express.static(path.join(__dirname, '../public')))
 // console.log(__dirname)
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '404.html'));
 })
 
 app.listen(3000, () => {
--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,7 +1,9 @@
 const controller = {}
+const title = 'INDEX DESDE EL SERVIDOR CON PUG Y DESDE UNA VARIABLE'
 
 controller.index = (req, res) => {
-        res.send('La conexión ha sido muy correcta desde index.controller')
+        // res.send('La conexión ha sido muy correcta desde index.controller')
+        res.render('index', {title})
 }
 
 module.exports = controller
